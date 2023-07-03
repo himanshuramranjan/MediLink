5d3a8f1948a44229b64ee2fcbe46436c4ad55514
himanshuramranjan <himanshuramr@gmail.com>
2023-07-03 19:18:19 +0530

    login functionality added to controller

--- controllers/authController.js
+++ controllers/authController.js
@@ -31,3 +31,28 @@ exports.signup = async (req, res) => {
         });
     }
 }
+
+// Log In existing an exsting user
+exports.login = async (req, res) => {
+
+    try {
+        const { username, password } = req.body;
+
+        if(!username || !password) {
+            throw "Please provide valid email and password";
+        }
+
+        const doctor = await Doctor.findOne({ username }).select('+password');
+
+        if(!doctor || !(await doctor.isCorrectPassword(password, doctor.password))) {
+            throw "Incorrect name or password";
+        }
+
+        sendJWTToken(res, doctor, 200);
+    } catch(err) {
+        console.log(err);
+        res.status(404).json({
+            status: 'fail'
+        })
+    }
+}
