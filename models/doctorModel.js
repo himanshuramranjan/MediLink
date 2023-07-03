832e594f0e64fb2de811036a7b6fdaa306436d35
himanshuramranjan <himanshuramr@gmail.com>
2023-07-03 17:55:22 +0530

    doctor model created

---
+++ models/doctorModel.js
@@ -0,0 +1,33 @@
+const mongoose = require('monoose');
+
+const doctorSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        unique: [true, 'This username already exist'],
+        required: [true, 'Username is required']
+    },
+    name: {
+        type: String,
+        required: [true, 'Name is required']
+    },
+    password: {
+        type: String,
+        required: [true, 'Password is required'],
+        minLength: [8, 'Password should be 8 chars long'],
+        select: false
+    },
+    confirmPassword: {
+        type: String,
+        required: [true, 'Password is required'],
+        validate: {
+            validator: function(confirmPswd) {
+                return this.password === confirmPswd;
+            },
+            message: 'Passwords are not matching'
+        }
+    }
+});
+
+const Doctor = mongoose.model('Dcotor', doctorSchema);
+
+module.exports = Doctor;
