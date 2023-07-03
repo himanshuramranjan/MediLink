aeb98f213450f05da85a5dd078a228655a0c09f6
himanshuramranjan <himanshuramr@gmail.com>
2023-07-03 18:34:46 +0530

    routes path defined

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 const express = require('express');
 
+const doctorRouter = require('./routes/doctorRoutes');
+const patientRouter = require('./routes/patientRoutes');
+const reportRouter = require('./routes/reportRoutes');
+
 const app = express();
 
-
-app.use('/', (req, res) => {
-    res.status(200).json({
-        msg: 'Teting'
-    })
-});
+app.use('/doctors', doctorRouter);
+app.use('/patients', patientRouter);
+app.use('/reports', reportRouter);
 
 module.exports = app;
