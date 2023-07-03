07760d9e5a7e01148b1f671ee9c06e3c0f18ee5e
himanshuramranjan <himanshuramr@gmail.com>
2023-07-03 20:28:28 +0530

    validation for mobile num added

--- models/patientModel.js
+++ models/patientModel.js
@@ -8,7 +8,13 @@ const patientSchema = new mongoose.Schema({
     mobile: {
         type: String,
         required: [true, 'Mobile number is required'],
-        unique: [true, 'This mobile number already exist']
+        unique: [true, 'This mobile number already exist'],
+        validate: {
+            validator: function() {
+                return this.mobile.length === 10;
+            },
+            message: 'Mobile number should be 10 digit long'
+        }
     }
 });
 
