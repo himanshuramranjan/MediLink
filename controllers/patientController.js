f74b94668408c6d62a6ff77e833507d8e5eb40f5
himanshuramranjan <himanshuramr@gmail.com>
2023-07-04 16:27:19 +0530

    comments updated

--- controllers/patientController.js
+++ controllers/patientController.js
@@ -3,6 +3,8 @@ const Patient = require('../models/patientModel');
 // Register a new patient
 exports.registerPatient = async (req, res) => {
     try {
+
+        // check for existing patient
         const existingPatient = await Patient.findOne({ mobile: req.body.mobile });
         
         if(existingPatient) {
@@ -14,6 +16,7 @@ exports.registerPatient = async (req, res) => {
             });
         }
         
+        // if no patient exist create a new one
         const patient = await Patient.create(req.body);
 
         return res.status(201).json({
