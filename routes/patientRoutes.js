903ae1460041f65169e7d0cf113a7de70fc7bb95
himanshuramranjan <himanshuramr@gmail.com>
2023-07-04 12:45:55 +0530

    report routes added

--- routes/patientRoutes.js
+++ routes/patientRoutes.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const authController = require('../controllers/authController');
 const patientController = require('../controllers/patientController');
+const reportRouter = require('./reportRoutes');
 
 const router = express.Router();
+
+router.use('/:id', reportRouter);
 
 router.post('/register', authController.protectRoute, patientController.registerPatient);
 
--- routes/reportRoutes.js
+++ routes/reportRoutes.js
@@ -1,5 +1,13 @@
 const express = require('express');
+const reportController = require('../controllers/reportController');
+const authController = require('../controllers/authController');
 
-const router = express.Router();
+const router = express.Router({ mergeParams: true });
+
+router.use(authController.protectRoute);
+
+router.post('/create-report', reportController.createReport);
+router.get('/all-reports', reportController.getAllReports);
+router.get('/:status', reportController.getReport);
 
 module.exports = router;
