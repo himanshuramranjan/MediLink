72728777f3419498c64ffe8002208bddb76ab2f1
himanshuramranjan <himanshuramr@gmail.com>
2023-07-04 13:31:44 +0530

    code updated

--- routes/reportRoutes.js
+++ routes/reportRoutes.js
@@ -1,11 +1,12 @@
 const express = require('express');
+
 const reportController = require('../controllers/reportController');
 const authController = require('../controllers/authController');
 
 const router = express.Router({ mergeParams: true });
 
 router.use(authController.protectRoute);
-
+ 
 router.post('/create-report', reportController.createReport);
 router.get('/all-reports', reportController.getAllReports);
 router.get('/:status', reportController.getReports);
