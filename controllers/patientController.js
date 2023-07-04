3ec31055f8e985d82e00e9379b07290d69fe02d3
himanshuramranjan <himanshuramr@gmail.com>
2023-07-04 16:21:09 +0530

    functionality to check existing Patient added

--- controllers/patientController.js
+++ controllers/patientController.js
@@ -3,6 +3,17 @@ const Patient = require('../models/patientModel');
 // Register a new patient
 exports.registerPatient = async (req, res) => {
     try {
+        const existingPatient = await Patient.findOne({ mobile: req.body.mobile });
+        
+        if(existingPatient) {
+            return res.status(201).json({
+                status: 'success',
+                data: {
+                    existingPatient
+                }
+            });
+        }
+        
         const patient = await Patient.create(req.body);
 
         return res.status(201).json({
